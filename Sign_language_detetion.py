d8d22a22ecf27f3cdc9bdd9823942f58b60de5f2
useless21 <123755636+useless21@users.noreply.github.com>
2024-04-13 02:07:36 +0800

    Add files via upload

---
+++ Sign_language_detetion.py
@@ -0,0 +1,299 @@
+import streamlit as st
+import mediapipe as mp
+import cv2
+import numpy as np
+import tempfile
+import time
+from PIL import Image
+
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+
+DEMO_VIDEO = 'demo.mp4'
+DEMO_IMAGE = 'demo.jpg'
+
+my_list = []
+
+
+st.title('Sign Language Detection Using MediaPipe with Streamlit GUI')
+
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 350px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 350px;
+        margin-left: -350px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.sidebar.title('Sign Language Detection - Sameer Edlabadkar')
+st.sidebar.subheader('-Parameter')
+
+@st.cache()
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+app_mode = st.sidebar.selectbox('Choose the App mode',
+['About App','Run on Image','Run on Video']
+)
+
+if app_mode =='About App':
+    st.markdown('In this application we are using **MediaPipe** for detecting Sign Language. **StreamLit** is to create the Web Graphical User Interface (GUI) ')
+    st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 400px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 400px;
+        margin-left: -400px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+    st.video('https://youtu.be/eLfqvVbvyzA')
+    st.markdown('''
+              # About Me \n 
+                Hey this is **Sameer Edlabadkar**. Working on the technologies such as **Tensorflow, MediaPipe, OpenCV, ResNet50**. \n
+
+                Also check me out on Social Media
+                - [YouTube](https://www.youtube.com/@edlabadkarsameer/videos)
+                - [LinkedIn](https://www.linkedin.com/in/sameer-edlabadkar-43b48b1a7/)
+                - [GitHub](https://github.com/edlabadkarsameer)
+              If you are facing any issue while working feel free to mail me on **edlabadkarsameer@gmail.com**
+
+                ''')
+elif app_mode == 'Run on Video':
+
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+
+    use_webcam = st.sidebar.button('Use Webcam')
+    record = st.sidebar.checkbox("Record Video")
+    if record:
+        st.checkbox("Recording", value=True)
+
+    st.sidebar.markdown('---')
+    sameer=""
+    st.markdown(' ## Output')
+    st.markdown(sameer)
+
+    stframe = st.empty()
+    video_file_buffer = st.sidebar.file_uploader("Upload a video", type=["mp4", "mov", 'avi', 'asf', 'm4v'])
+    tfflie = tempfile.NamedTemporaryFile(delete=False)
+
+    if not video_file_buffer:
+        if use_webcam:
+            vid = cv2.VideoCapture(0)
+        else:
+            vid = cv2.VideoCapture(DEMO_VIDEO)
+            tfflie.name = DEMO_VIDEO
+
+    else:
+        tfflie.write(video_file_buffer.read())
+        vid = cv2.VideoCapture(tfflie.name)
+
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps_input = int(vid.get(cv2.CAP_PROP_FPS))
+
+    codec = cv2.VideoWriter_fourcc('V', 'P', '0', '9')
+    out = cv2.VideoWriter('output1.mp4', codec, fps_input, (width, height))
+
+    st.markdown("<hr/>", unsafe_allow_html=True)
+
+    st.sidebar.markdown('---')
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+            width: 400px;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+            width: 400px;
+            margin-left: -400px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    finger_tips = [8, 12, 16, 20]
+    thumb_tip = 4
+    while True:
+        ret, img = vid.read()
+        img = cv2.flip(img, 1)
+        h, w, c = img.shape
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(img)
+
+        img.flags.writeable = True
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
+        if results.multi_hand_landmarks:
+            for hand_landmark in results.multi_hand_landmarks:
+                lm_list = []
+                for id, lm in enumerate(hand_landmark.landmark):
+                    lm_list.append(lm)
+                finger_fold_status = []
+                for tip in finger_tips:
+                    x, y = int(lm_list[tip].x * w), int(lm_list[tip].y * h)
+                    # print(id, ":", x, y)
+                    # cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+
+                    if lm_list[tip].x < lm_list[tip - 2].x:
+                        # cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+                        finger_fold_status.append(True)
+                    else:
+                        finger_fold_status.append(False)
+
+                print(finger_fold_status)
+                x, y = int(lm_list[8].x * w), int(lm_list[8].y * h)
+                print(x, y)
+                # fuck off
+                if lm_list[3].x < lm_list[4].x and lm_list[8].y > lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
+                    cv2.putText(img, "fuck off !!!", (200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    sameer="fuck off"
+
+                # one
+                if lm_list[3].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
+                    cv2.putText(img, "ONE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("1")
+                    sameer="one"
+
+                # two
+                if lm_list[3].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
+                    cv2.putText(img, "TWO", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("2")
+                    sameer="two"
+                # three
+                if lm_list[2].x < lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y:
+                    cv2.putText(img, "THREE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("3")
+                    sameer="three"
+
+                # four
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[2].x < lm_list[8].x:
+                    cv2.putText(img, "FOUR", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("4")
+                    sameer="Four"
+
+                # five
+                if lm_list[2].x < lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "FIVE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("5")
+                    sameer="Five"
+                    # six
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "SIX", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("6")
+                    sameer="Six"
+                # SEVEN
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "SEVEN", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("7")
+                    sameer="Seven"
+                # EIGHT
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "EIGHT", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("8")
+                    sameer="Eight"
+                # NINE
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y > lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "NINE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("9")
+                    sameer="Nine"
+                # A
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y > lm_list[6].y and lm_list[12].y > lm_list[10].y and \
+                        lm_list[16].y > lm_list[14].y and lm_list[20].y > lm_list[18].y and lm_list[17].x < lm_list[
+                    0].x < \
+                        lm_list[5].x:
+                    cv2.putText(img, "A", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("A")
+                    sameer="A"
+                # B
+                if lm_list[2].x > lm_list[4].x and lm_list[8].y < lm_list[6].y and lm_list[12].y < lm_list[10].y and \
+                        lm_list[16].y < lm_list[14].y and lm_list[20].y < lm_list[18].y and lm_list[2].x > lm_list[8].x:
+                    cv2.putText(img, "B", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    my_list.append("B")
+                    sameer="B"
+            mp_draw.draw_landmarks(img, hand_landmark,
+                                   mp_hands.HAND_CONNECTIONS,
+                                   mp_draw.DrawingSpec((0, 0, 255), 6, 3),
+                                   mp_draw.DrawingSpec((0, 255, 0), 4, 2)
+                                   )
+            if record:
+                # st.checkbox("Recording", value=True)
+                out.write(img)
+            # Dashboard
+
+            frame = cv2.resize(img, (0, 0), fx=0.8, fy=0.8)
+            frame = image_resize(image=frame, width=640)
+            stframe.image(frame, channels='BGR', use_column_width=True)
+
+    st.text('Video Processed')
+
+    output_video = open('output1.mp4', 'rb')
+    out_bytes = output_video.read()
+    st.video(out_bytes)
+
+    vid.release()
+    out.release()
